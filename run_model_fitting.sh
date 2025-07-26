8476c09620d8b3c44d9db34f2ec3851c39ad71c7
Tanishq Patil <tanishqpatil.iitk@gmail.com>
2025-07-26 23:27:17 +0100

    Script for executing the model training and inference process

---
+++ run_model_fitting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir_name=$1
+
+# Generate timestamp
+timestamp=$(date +%Y%m%d_%H%M%S)
+
+# Create a directory named after the timestamp
+mkdir -p "$dir_name/model_output_directory/$timestamp"
+
+# Run the Python script, passing the timestamp as an argument
+# Redirect output to a log file inside the timestamped directory
+python $dir_name/main.py "$timestamp" > "$dir_name/model_output_directory/$timestamp/model_fitting_output_$timestamp.log" 2>&1
